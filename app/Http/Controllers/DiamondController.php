51089457e439ea74b16c51a0c6b4baefabfc1aaf
Rahul Sharma <rahul.hcst@outlook.com>
2016-12-09 04:38:57 +0530

    Change

--- app/Http/Controllers/DiamondController.php
+++ app/Http/Controllers/DiamondController.php
@@ -119,6 +119,7 @@ class DiamondController extends Controller
                 }
 
                 if (strlen($text) < $limit) {                               //For the text UNOCOIN
+                    var_dump(strlen($unoText));
                     $unoText = str_repeat($unoText, floor($limit/strlen($unoText)));
                     for ($k = 0; $k < strlen($unoText); $k++) {
                         $appResult = $this->append($text, $limit, $unoText[$k]);
